75175e47313821c9723479d4e92c561c644acedd
Ulisses <ulissessuporteti01@gmail.com>
2023-02-25 08:19:11 -0300

    primeiro commit - enviando todos os arquivos

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useToast } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  const toast = useToast();
+
+  return <div className="App">Hello World!</div>;
+}
+
+export default App;
