1e8c684537309233dc0be8409b77a48efaf0c1f7
Ulisses <ulissessuporteti01@gmail.com>
2023-02-25 10:29:10 -0300

    feat: adicionando o reset do css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
-import './index.css'
+import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
